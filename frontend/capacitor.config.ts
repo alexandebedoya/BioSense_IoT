1833ada5bcc7bc7d25cef7dc5e31e21caddd93a3
alexandebedoya <alexis10129706@gmail.com>
2026-03-22 01:44:27 -0500

    fix: incluir gradle-wrapper para Appflow y config Google

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.biosense.iot',
   appName: 'BioSense IoT',
-  webDir: 'out'
+  webDir: 'out',
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: '669903110693-q703kiablp1rvq509996i5k3dqpbvhml.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true,
+    },
+  },
 };
 
 export default config;
